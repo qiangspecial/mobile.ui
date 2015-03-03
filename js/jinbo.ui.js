51760474ae46d40169a671c57dfa408c476d5c6e
jinbo <qiangspecial@qq.com>
2015-03-03 23:09:23 +0800

    mobile.ui 完善, ui.dialog 初步完成

--- js/jinbo.ui.js
+++ js/jinbo.ui.js
@@ -1,12 +1,11 @@
 var ui = ui || {};
 
-
 /**
  * 弹出对话框插件
  * 调用方法: ui.dialog(o);
  * o: {}
  */
-ui.dialog = (function($) {
+(function(ui, $) {
 	// dialog 默认参数
 	var _option = {
 		content: "",			// String   弹出对话框的内容
@@ -17,92 +16,45 @@ ui.dialog = (function($) {
 		beforeClose: null, 
 		callback: null			// Function 关闭弹出层执行的回调函数
 	},
-	_getOverlay = function() {
-		var $overlay = $("#ui-overlay");
+	_getOverlay = function(needOverlay) {
+		var $overlay = $("#ui-overlay"),
+			// 判断是否需要遮罩层(遮罩始终存在,只是显示为 灰色/透明 之分)
+			overlayContent = '<div id="ui-overlay" class="' + (needOverlay ? 'ui-overlay ' : '') + 'ui-fixed-center"></div>';
 
-		// if ($overlay[0]) {
-		// 	$overlay.empty();
-		// } else {
-		// 	$overlay = $('<div id="ui-overlay" class="ui-overlay"></div>');
-		// }
-		return $overlay[0] ? $overlay.empty() : $('<div id="ui-overlay" class="ui-overlay"></div>');
+		// 删除原有的弹出框
+		$overlay[0] && $overlay.remove();
+		return $(overlayContent);
 	};
 
-	var _fnDialog = function(o) {
-		var $overlay = _getOverlay(),
+	// 默认显示的对话框
+	_option.content = '<div class="ui-dialog ui-center">' +
+		'<h4 class="ui-dialog-title">财宝宝提示：</h4>' +
+		'<p class="text-center fz14">密码错误密码错误密码错误密码错误密码错误密码错误密码错误密码错误</p>' +
+		'<div class="ui-btn-group mgt15">' +
+			'<a href="javascript:;" class="ui-btn">确认</a>' +
+			'<a href="javascript:;" class="ui-btn" data-type="close">取消</a>' +
+		'</div>' +
+	'</div>';
+	ui.dialog = function(o) {
+		var $overlay,
 			dialog = {};
 
-		o = $.extend({}, _option, o);	// 合并参数
+		o = $.extend({}, _option, o);			// 合并参数
+		$overlay = _getOverlay(o.needOverlay); 	// 获取遮罩
 
 		$("body").append($overlay.html(o.content));
+		// 禁止滑动页面
 		$overlay.show(200).on("touchmove", function(e) {
 			e.preventDefault();
 		});
 		dialog.$ = $overlay;
 		dialog.close = function() {
 			o.callback && o.callback.call($overlay[0]); // 执行回调函数
-			$overlay.hide(function() {
-				$overlay.remove();
-			});
+			$overlay.remove();
 		};
-
-		$overlay.on("touchend", ".close", dialog.close);
+		// 添加关闭btn
+		$overlay.on("touchend", "[data-type='close']", dialog.close);
 		return dialog;
 	};
+}(ui, typeof Zepto !== "undefined" ? Zepto : jQuery));
 
-	return _fnDialog;
-}(typeof Zepto !== "undefined" ? Zepto : jQuery));
-
-
-/* 弹出对话框插件 */
-;(function($) {
-	var _option = {
-		html: "",					// string   弹出对话框的内容
-		container: null,			// string   弹处层内容的id
-		needOverlay: true,			// boolean  是否添加遮罩层
-		autoClose: 0,				// number   自动关闭弹出层的时间
-		callback: null				// function 关闭弹出层执行的回调函数
-	},
-	_$notNeedOverlay = null;
-
-	$.extend($, {
-		dialog: function(option) {
-			var o = $.extend({}, _option, option),
-				$overlay = function() {
-					var _overlay = $("#overlay");
-
-					if (_$notNeedOverlay) {
-						_$notNeedOverlay.remove();
-						_$notNeedOverlay = null;
-					}
-					if (o.needOverlay) {
-
-						return _overlay[0] ? _overlay.empty().html(o.html) : $('<div id="overlay" class="overlay"></div>').html(o.html);
-					} else {
-						if (_overlay[0]) _overlay.remove();
-						return (_$notNeedOverlay = $(o.html));
-					}
-				}(),
-				dialog = {			// 开放给外部的接口
-					$: null,		// {$()} 弹出对话框对象
-					close: null		// {function} 关闭事件
-				};
-
-			$("body").append($overlay);
-			$overlay.show(200).on("touchmove", function(e) {
-				e.preventDefault();
-			});
-			dialog.$ = $overlay;
-			dialog.close = function() {
-
-				o.callback && o.callback.call($overlay[0]);	// 执行回调函数
-				$overlay.animate({"opacity": 0}, 100, function() {
-					$overlay.remove();
-				});
-			};
-
-			$overlay.on("click", ".close", dialog.close);
-			return dialog;
-		}
-	});
-}(((typeof jQuery !== "undefined") ? jQuery : Zepto)));
